48c398ba1c9ef7a7621282c52056c0c0c40d39cb
nac-39 <83295760+nac-39@users.noreply.github.com>
2024-10-22 18:08:26 +0900

    fmt

--- src/main.py
+++ src/main.py
@@ -20,40 +20,34 @@ if __name__ == "__main__":
     n_samples = len(dataset)
     train_size = int(n_samples * 0.8)
     val_size = n_samples - train_size
+
     train_data, test_data = random_split(dataset, [train_size, val_size])
 
-    if train:
-        train_loader = DataLoader(
-            train_data, batch_size=32, shuffle=True, pin_memory=True
-        )
-        bert_model = BERT(
-            vocab_size=len(tokenizer.vocab),
-            d_model=768,
-            n_layers=2,
-            heads=12,
-            dropout=0.1,
-        )
+    train_loader = DataLoader(train_data, batch_size=32, shuffle=True, pin_memory=True)
+    test_loader = DataLoader(test_data, batch_size=32, shuffle=False, pin_memory=True)
 
-        bert_lm = BERTLM(bert_model, len(tokenizer.vocab))
+    bert_model = BERT(
+        vocab_size=len(tokenizer.vocab),
+        d_model=768,
+        n_layers=2,
+        heads=12,
+        dropout=0.1,
+    )
+    bert_lm = BERTLM(bert_model, len(tokenizer.vocab))
+
+    if train:
         bert_trainer = BERTTrainer(bert_lm, train_loader, device="cuda")
         epochs = 1
 
-    for epoch in range(epochs):
-        bert_trainer.train(epoch)
+        for epoch in range(epochs):
+            bert_trainer.train(epoch)
     else:
-        test_loader = DataLoader(
-            test_data, batch_size=32, shuffle=False, pin_memory=True
+        bert_trainer = BERTTrainer(
+            bert_lm,
+            train_dataloader=train_loader,
+            test_dataloader=test_loader,
+            device="cuda",
         )
-        bert_model = BERT(
-            vocab_size=len(tokenizer.vocab),
-            d_model=768,
-            n_layers=2,
-            heads=12,
-            dropout=0.1,
-        )
-
-        bert_lm = BERTLM(bert_model, len(tokenizer.vocab))
-        bert_trainer = BERTTrainer(bert_lm,train_dataloader=train_loader, test_dataloader=test_loader, device="cuda")
         bert_trainer.model.load_state_dict(torch.load("bert_pretrain_epoch_0.pt"))
         epochs = 1
 
